996bc697a88e81d33b5b72b768186eac095f3cff
Lnvo <Lnvo@DESKTOP-98HBNMI>
2025-04-23 12:34:20 +0330

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int n = 10;
+            int a = 0, b = 1;
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(a);
+                int temp = a + b;
+                a = b;
+                b = temp;
+            }
+
+            Console.ReadLine();
+        }
+
+
+
+
+    }
+
+        
+
+}
+
+           
